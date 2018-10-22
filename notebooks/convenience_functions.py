e207e494fac4e12449e18bcc5047cbcad81da927
Matt Craig <mattwcraig@gmail.com>
2018-10-21 22:15:57 -0500

    Add an input_ratio argument and make pep8 fixes

--- notebooks/convenience_functions.py
+++ notebooks/convenience_functions.py
@@ -5,7 +5,8 @@ from matplotlib import pyplot as plt
 
 def show_image(image, percl=99, percu=None, figsize=(6, 10),
                cmap='viridis', log=False,
-               show_colorbar=True, show_ticks=True):
+               show_colorbar=True, show_ticks=True,
+               fig=None, ax=None, input_ratio=None):
     """
     Show an image in matplotlib with some basic astronomically-appropriat stretching.
 
@@ -22,20 +23,24 @@ def show_image(image, percl=99, percu=None, figsize=(6, 10),
     """
     if percu is None:
         percu = percl
-        percl = 100-percl
+        percl = 100 - percl
 
-    if figsize is not None:
-        # Rescale the fig size to match the image dimensions, roughly
-        image_aspect_ratio = image.shape[0]/image.shape[1]
-        figsize = (max(figsize) * image_aspect_ratio, max(figsize))
+    if (fig is None and ax is not None) or (fig is not None and ax is None):
+        raise ValueError('Must provide both "fig" and "ax" '
+                         'if you provide one of them')
+    elif fig is None and ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+        if figsize is not None:
+            # Rescale the fig size to match the image dimensions, roughly
+            image_aspect_ratio = image.shape[0] / image.shape[1]
+            figsize = (max(figsize) * image_aspect_ratio, max(figsize))
+            print(figsize)
 
-    fig, ax = plt.subplots(1,1, figsize=figsize)
+    # To preserve details we should *really* downsample correctly and
+    # not rely on matplotlib to do it correctly for us (it won't).
 
-    # To preserve details we should *really* downsample correctly and not rely on
-    # matplotlib to do it correctly for us (it won't).
-
-    # So, calculate the size of the figure in pixels, block_reduce to roughly that,
-    # and display the block reduced image.
+    # So, calculate the size of the figure in pixels, block_reduce to
+    # roughly that,and display the block reduced image.
 
     # Thanks, https://stackoverflow.com/questions/29702424/how-to-get-matplotlib-figure-size
     fig_size_pix = fig.get_size_inches() * fig.dpi
@@ -45,23 +50,38 @@ def show_image(image, percl=99, percu=None, figsize=(6, 10),
     if ratio < 1:
         ratio = 1
 
-    # Divide by the square of the ratio to keep the flux the same in the reduced image
+    ratio = input_ratio or ratio
+
+    # Divide by the square of the ratio to keep the flux the same in the
+    # reduced image
     reduced_data = block_reduce(image, ratio) / ratio**2
 
-    # Of course, now that we have downsampled, the axis limits are changed to match
-    # the smaller image size. Setting the extent will do the trick to change the axis display
-    # back to showing the actual extent of the image.
+    # Of course, now that we have downsampled, the axis limits are changed to
+    # match the smaller image size. Setting the extent will do the trick to
+    # change the axis display back to showing the actual extent of the image.
     extent = [0, image.shape[1], 0, image.shape[0]]
+
     if log:
         stretch = aviz.LogStretch()
     else:
         stretch = aviz.LinearStretch()
+
     norm = aviz.ImageNormalize(reduced_data, interval=aviz.AsymmetricPercentileInterval(percl, percu),
                                       stretch=stretch)
 
-    im = ax.imshow(reduced_data, norm=norm, origin='lower', cmap=cmap, extent=extent)
+    im = ax.imshow(reduced_data, norm=norm, origin='lower',
+                   cmap=cmap, extent=extent)
+
     if show_colorbar:
-        plt.colorbar(im)
+        # I haven't a clue why the fraction and pad arguments below work to make
+        # the colorbar the same height as the image, but they do....unless the image
+        # is wider than it is tall. Sticking with this for now anyway...
+        # Thanks: https://stackoverflow.com/a/26720422/3486425
+        fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+        # In case someone in the future wants to improve this:
+        # https://joseph-long.com/writing/colorbars/
+        # https://stackoverflow.com/a/33505522/3486425
+        # https://matplotlib.org/mpl_toolkits/axes_grid/users/overview.html#colorbar-whose-height-or-width-in-sync-with-the-master-axes
 
     if not show_ticks:
         ax.tick_params(labelbottom=False, labelleft=False)
