045feff1e0c9900531f7149261ab66a4a36b4d94
Matt Craig <mattwcraig@gmail.com>
2019-06-22 22:49:13 -0500

    Complete draft of cosmic ray removal

--- notebooks/convenience_functions.py
+++ notebooks/convenience_functions.py
@@ -3,7 +3,9 @@ from astropy.nddata.utils import block_reduce
 from matplotlib import pyplot as plt
 
 
-def show_image(image, percl=99, percu=None, figsize=(6, 10),
+def show_image(image,
+               percl=99, percu=None, is_mask=False,
+               figsize=(6, 10),
                cmap='viridis', log=False,
                show_colorbar=True, show_ticks=True,
                fig=None, ax=None, input_ratio=None):
@@ -69,19 +71,149 @@ def show_image(image, percl=99, percu=None, figsize=(6, 10),
     norm = aviz.ImageNormalize(reduced_data, interval=aviz.AsymmetricPercentileInterval(percl, percu),
                                       stretch=stretch)
 
-    im = ax.imshow(reduced_data, norm=norm, origin='lower',
-                   cmap=cmap, extent=extent)
+    if is_mask:
+        # The image is a mask in which pixels are zero or one. Set the image scale
+        # limits appropriately.
+        scale_args = dict(vmin=0, vmax=1)
+    else:
+        scale_args = dict(norm=norm)
+
+    im = ax.imshow(reduced_data, origin='lower',
+                   cmap=cmap, extent=extent, aspect='equal', **scale_args)
 
     if show_colorbar:
         # I haven't a clue why the fraction and pad arguments below work to make
         # the colorbar the same height as the image, but they do....unless the image
         # is wider than it is tall. Sticking with this for now anyway...
         # Thanks: https://stackoverflow.com/a/26720422/3486425
-        fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
+        fig.colorbar(im, ax=ax, fraction=0.046, pad=0.04, format='%2.0f')
         # In case someone in the future wants to improve this:
         # https://joseph-long.com/writing/colorbars/
         # https://stackoverflow.com/a/33505522/3486425
         # https://matplotlib.org/mpl_toolkits/axes_grid/users/overview.html#colorbar-whose-height-or-width-in-sync-with-the-master-axes
 
     if not show_ticks:
-        ax.tick_params(labelbottom=False, labelleft=False)
+        ax.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False)
+
+
+def image_snippet(image, center, width=50, axis=None, fig=None, is_mask=False):
+    """
+    Display a subsection of an image about a center.
+
+    Parameters
+    ----------
+
+    image : numpy array
+        The full image from which a section is to be taken.
+
+    center : list-like
+        The location of the center of the cutout.
+
+    width : int, optional
+        Width of the cutout, in pixels.
+
+    axis : matplotlib.Axes instance, optional
+        Axis on which the image should be displayed.
+
+    fig : matplotlib.Figure, optional
+        Figure on which the image should be displayed.
+
+    is_misk : bool, optional
+        Set to ``True`` if the image is a mask, i.e. all values are
+        either zero or one.
+    """
+    x_slice = slice(max(center[1] - width // 2, 0), center[1] + width // 2)
+    y_slice = slice(max(center[0] - width // 2, 0), center[0] + width // 2)
+    sub_image = image[x_slice, y_slice]
+    show_image(sub_image, cmap='gray', ax=axis, fig=fig,
+               show_colorbar=False, show_ticks=False, is_mask=is_mask)
+
+
+def _mid(sl):
+    return (sl.start + sl.stop) // 2
+
+
+def display_cosmic_rays(cosmic_rays, images, titles=None,
+                        only_display_rays=None):
+    """
+    Display cutouts of the region around each cosmic ray and the other images
+    passed in.
+
+    Parameters
+    ----------
+
+    cosmic_rays : photutils.segmentation.SegmentationImage
+        The segmented cosmic ray image returned by ``photuils.detect_source``.
+
+    images : list of images
+        The list of images to be displayed. Each image becomes a column in
+        the generated plot. The first image must be the cosmic ray mask.
+
+    titles : list of str
+        Titles to be put above the first row of images.
+
+    only_display_rays : list of int, optional
+        The number of the cosmic ray(s) to display. The default value,
+        ``None``, means display them all. The number of the cosmic ray is
+        its index in ``cosmic_rays``, which is also the number displayed
+        on the mask.
+    """
+    # Check whether the first image is actually a mask.
+
+    if not ((images[0] == 0) | (images[0] == 1)).all():
+        raise ValueError('The first image must be a mask with '
+                         'values of zero or one')
+
+    if only_display_rays is None:
+        n_rows = len(cosmic_rays.slices)
+    else:
+        n_rows = len(only_display_rays)
+
+    n_columns = len(images)
+
+    width = 12
+
+    # The height below is *CRITICAL*. If the aspect ratio of the figure as
+    # a whole does not allow for square plots then one ends up with a bunch
+    # of whitespace. The plots here are square by design.
+    height = width / n_columns * n_rows
+    fig, axes = plt.subplots(n_rows, n_columns, sharex=False, sharey='row',
+                             figsize=(width, height))
+
+    # Generate empty titles if none were provided.
+    if titles is None:
+        titles = [''] * n_columns
+
+    display_row = 0
+
+    for row, s in enumerate(cosmic_rays.slices):
+        if only_display_rays is not None:
+            if row not in only_display_rays:
+                # We are not supposed to display this one, so skip it.
+                continue
+
+        x = _mid(s[1])
+        y = _mid(s[0])
+
+        for column, plot_info in enumerate(zip(images, titles)):
+            image = plot_info[0]
+            title = plot_info[1]
+            is_mask = column == 0
+            ax = axes[display_row, column]
+            image_snippet(image, (x, y), width=80, axis=ax, fig=fig,
+                          is_mask=is_mask)
+            if is_mask:
+                ax.annotate('Cosmic ray {}'.format(row), (0.1, 0.9),
+                            xycoords='axes fraction',
+                            color='cyan', fontsize=20)
+
+            if display_row == 0:
+                # Only set the title if it isn't empty.
+                if title:
+                    ax.set_title(title)
+
+        display_row = display_row + 1
+
+    # This choice results in the images close to each other but with
+    # a small gap.
+    plt.subplots_adjust(wspace=0.1, hspace=0.05)
