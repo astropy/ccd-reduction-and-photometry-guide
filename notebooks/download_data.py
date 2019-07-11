2398f6cb202f657e8796f1106242d90c93fbece2
Matthew Craig <mattwcraig@gmail.com>
2019-07-11 15:03:45 -0500

    First draft of script for downloading data used by the notebooks

---
+++ notebooks/download_data.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import tarfile
+
+from astropy.utils.data import download_file
+
+# Get single images
+url = 'https://zenodo.org/record/3320113/files/combined_bias_100_images.fit.bz2?download=1'
+download = download_file(url, show_progress=True)
+p = Path(download)
+p.rename('combined_bias_100_images.fit.bz2')
+
+url = 'https://zenodo.org/record/3312535/files/dark-test-0002d1000.fit.bz2?download=1'
+download = download_file(url, show_progress=True)
+p = Path(download)
+p.rename('dark-test-0002d1000.fit.bz2')
+
+url = 'https://zenodo.org/record/3332818/files/combined_dark_300.000.fits.bz2?download=1'
+download = download_file(url, show_progress=True)
+p = Path(download)
+p.rename('combined_dark_300.000.fits.bz2')
+
+
+# Get the tarball for the smaller example
+url = 'https://zenodo.org/record/3254683/files/example-cryo-LFC.tar.bz2?download=1'
+download = download_file(url, show_progress=True, cache=True)
+tarball = tarfile.open(download)
+tarball.extractall('.')
+
+# Get the tarball for the bigger example
+url = 'https://zenodo.org/record/3245296/files/example-thermo-electric.tar.bz2?download=1'
+download = download_file(url, show_progress=True, cache=True)
+tarball = tarfile.open(download)
+tarball.extractall('.')
