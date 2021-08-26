a4843bcdcd56703338605387568216574705b309
Matt Craig <mattwcraig@gmail.com>
2021-08-26 11:03:41 -0500

    Add more comments to download script
    Really the intent of this is to trigger a re-download of data in the
    github action by invalidating the github cache...

--- notebooks/download_data.py
+++ notebooks/download_data.py
@@ -24,13 +24,13 @@ download = download_file(url, show_progress=True)
 p = Path(download)
 p.rename('combined_dark_exposure_1000.0.fit.bz2')
 
-# Get the tarball for the smaller example
+# Get the tarball for the smaller example and extract it
 url = 'https://zenodo.org/record/3254683/files/example-cryo-LFC.tar.bz2?download=1'
 download = download_file(url, show_progress=True, cache=True)
 tarball = tarfile.open(download)
 tarball.extractall('.')
 
-# Get the tarball for the bigger example
+# Get the tarball for the bigger example and extract it
 url = 'https://zenodo.org/record/3245296/files/example-thermo-electric.tar.bz2?download=1'
 download = download_file(url, show_progress=True, cache=True)
 tarball = tarfile.open(download)
